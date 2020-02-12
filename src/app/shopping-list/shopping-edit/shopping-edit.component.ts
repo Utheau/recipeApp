0e696f330369a24d7b1bd797de2deb73e2fcd6ed
Antonio Paolo Mecci <antoniopaolomecci@gmail.com>
2020-02-12 17:16:11 +0100

    completed forms part (add-edit-delete)

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,14 +1,14 @@
-import { Subscription } from 'rxjs';
 import {
   Component,
   OnInit,
   OnDestroy,
   ViewChild
 } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
-import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -39,10 +39,6 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
       );
   }
 
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
-  }
-
   onSubmit(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
@@ -63,6 +59,10 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   onDelete() {
     this.slService.deleteIngredient(this.editedItemIndex);
     this.onClear();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
