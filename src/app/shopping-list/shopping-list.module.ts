6a8ce592b2b6658c9b3494299c47e25058b176ef
Antonio Paolo Mecci <antoniopaolomecci@gmail.com>
2020-02-17 17:20:44 +0100

    refactoring application modules

---
+++ src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,21 @@
+import { SharedModule } from './../shared/shared.module';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from './shopping-list.component';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  declarations: [
+    ShoppingListComponent,
+    ShoppingEditComponent
+  ],
+  imports: [
+    SharedModule,
+    FormsModule,
+    RouterModule.forChild([
+      { path: 'shopping-list', component: ShoppingListComponent },
+    ])
+  ]
+})
+export class ShoppingListModule { }
