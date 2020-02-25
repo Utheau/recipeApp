d6f6b5488a55e3900e8d9157284bc802aed75126
paolo <antoniopaolomecci@gmail.com>
2020-02-25 23:45:43 +0100

    quick fix

--- src/app/recipes/store/recipe.actions.ts
+++ src/app/recipes/store/recipe.actions.ts
@@ -3,6 +3,9 @@ import { Action } from '@ngrx/store';
 
 export const SET_RECIPES = '[Recipes] Set Recipes';
 export const FETCH_RECIPES = '[Recipes] Fetch Recipes';
+export const ADD_RECIPE = '[Recipes] Add Recipe';
+export const UPDATE_RECIPE = '[Recipes] Update Recipe';
+export const DELETE_RECIPE = '[Recipes] Delete Recipe';
 
 export class SetRecipes implements Action {
     readonly type = SET_RECIPES;
